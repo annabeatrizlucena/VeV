3a0862f7620859f1b2c3d4aeb06ba3b225c8a0e7
Andrielly Lucena <andrielly.lucena@ccc.ufcg.edu.br>
2023-08-17 20:12:53 -0300

    Adiciona funcionalidade de calculo de imposto da nota fiscal

--- andrielly-implementacao/gerenciadorNotaFiscal/src/main/GeradorNotaFiscal.java
+++ andrielly-implementacao/gerenciadorNotaFiscal/src/main/GeradorNotaFiscal.java
@@ -2,8 +2,8 @@ package main;
 
 public class GeradorNotaFiscal {
 
-	public NotaFiscal geraNota(String string, String string2, String string3, double d) {
-		NotaFiscal nota = new NotaFiscal();
+	public NotaFiscal geraNota(String nome, String endereco, String tipoServico, double valorFatura) {
+		NotaFiscal nota = new NotaFiscal(nome, endereco, tipoServico, valorFatura);
 		return nota;
 	}
 
