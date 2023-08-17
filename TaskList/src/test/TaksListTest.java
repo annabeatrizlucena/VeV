659aa7a38ecf0b652027b943e0dff79eac0bfb9c
Ana Lira <anna.lira@ccc.ufcg.edu.br>
2023-08-17 19:08:14 -0300

    finished Task List tests

--- TaskList/src/test/TaksListTest.java
+++ TaskList/src/test/TaksListTest.java
@@ -61,25 +61,80 @@ class TaksListTest {
 				+ "Priority: HIGH,\n");
 		taskList.deleteTask(taskId);
 		assertEquals(taskList.listTasks(), "");
-		
-		
 	}
 	
 	@Test
     public void testSetTaskPriority() {
         Task task = new Task("Test Task List", "Implement tests for the task list manager", "2023-08-21", Priority.HIGH);
-        taskManager.createTask(task);
+        taskList.createTask(task);
 
-        taskManager.setTaskPriority(task.getId(), Priority.MEDIUM);
+        taskList.setTaskPriority(task.getId(), Priority.MEDIUM);
 
-        assertEquals(Priority.MEDIUM, task.getPriority());
+        assertEquals(task.getPriority(), Priority.MEDIUM);
     }
-	//@Test
-	//void testUpdateCreatedTask() {
-	//	Task newTask = new Task("Test Task List", "Implement tests for the task list manager", "2023-08-21", Priority.HIGH);
-	//	taskList.createTask(newTask);	
-	//}
-
 	
+	@Test
+	void testUpdateCreatedTask() {
+		Task newTask = new Task("Test Task List", "Implement tests for the task list manager", "2023-08-21", Priority.HIGH);
+		int taskId = taskList.createTask(newTask);
+		assertEquals(taskList.listTasks(), "Title: Test Task List\n"
+				+ "Description: Implement tests for the task list manager\n"
+				+ "Due date: 2023-08-21\n"
+				+ "Priority: HIGH,\n");
+		
+		taskList.updateTask(taskId, "New Test task list", "Upgrade tests for the task list manager\n", "2023-08-20", Priority.LOW);
+		assertEquals(taskList.listTasks(), "New Test task list\n"
+				+ "Description: Upgrade tests for the task list manager\n"
+				+ "Due date: 2023-08-20\n"
+				+ "Priority: LOW,\n");
+	}
 	
+	@Test
+	public void testListTasksByDateAndPriorityLevelEqual() {
+		Task task1 = new Task("Test Task List", "Implement tests for the task list manager", "2023-08-21", Priority.HIGH);
+		int taskId1 = taskList.createTask(task1);
+        Task task2 = new Task("Test Task", "Implement tests for the task entity", "2023-08-30", Priority.HIGH);
+        int taskId2 = taskList.createTask(task2);
+        assertEquals(taskList.listTasks(), "Title: Test Task List\n"
+				+ "Description: Implement tests for the task list manager\n"
+				+ "Due date: 2023-08-21\n"
+				+ "Priority: HIGH,\n"
+				+ "Title: Test Task\n"
+				+ "Description: Implement tests for the task entity\n"
+				+ "Due date: 2023-08-30\n"
+				+ "Priority: HIGH,\n");
+	}
+	
+	@Test
+	public void testListTasksByDateAndPriorityLevelNotEqual() {
+		Task task1 = new Task("Test Task List", "Implement tests for the task list manager", "2023-08-21", Priority.LOW);
+		int taskId1 = taskList.createTask(task1);
+        Task task2 = new Task("Test Task", "Implement tests for the task entity", "2023-08-30", Priority.HIGH);
+        int taskId2 = taskList.createTask(task2);
+        assertEquals(taskList.listTasks(), "Title: Test Task List\n"
+				+ "Description: Implement tests for the task list manager\n"
+				+ "Due date: 2023-08-21\n"
+				+ "Priority: HIGH,\n"
+				+ "Title: Test Task\n"
+				+ "Description: Implement tests for the task entity\n"
+				+ "Due date: 2023-08-30\n"
+				+ "Priority: HIGH,\n");
+	}
+	
+	@Test
+	public void testSetTaskPriority() {
+		Task task = new Task("Test Task List", "Implement tests for the task list manager", "2023-08-21", Priority.LOW);
+		int taskId = taskList.createTask(task);
+		assertEquals(taskList.listTasks(), "Title: Test Task List\n"
+				+ "Description: Implement tests for the task list manager\n"
+				+ "Due date: 2023-08-21\n"
+				+ "Priority: LOW,\n");
+		taskList.setTaskPriority(taskId, Priority.HIGH);
+		assertEquals(taskList.listTasks(), "Title: Test Task List\n"
+				+ "Description: Implement tests for the task list manager\n"
+				+ "Due date: 2023-08-21\n"
+				+ "Priority: HIGH,\n");
+		
+		
+	}
 }
