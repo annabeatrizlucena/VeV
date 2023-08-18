6547e5d64cfc540704077f7ffe827be8a109e2d3
Ana Lira <anna.lira@ccc.ufcg.edu.br>
2023-08-17 21:59:45 -0300

    resolving beforeEach in TaskListTest

---
+++ TaskList/src/main/TaskList.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+
+	private List<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+}
