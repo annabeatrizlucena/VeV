d6076afc18683e091465e3e0509a1ad1193f3b54
Ana Lira <anna.lira@ccc.ufcg.edu.br>
2023-08-17 21:40:24 -0300

    convert task id type

--- TaskList/src/test/TaksListTest.java
+++ TaskList/src/test/TaksListTest.java
@@ -31,14 +31,14 @@ class TaksListTest {
 	@Test
 	void testTaskListSizeAfterCreateTask() {
 		Task newTask = new Task("Test Task List", "Implement tests for the task list manager", LocalDate.parse("2023-08-21", DateTimeFormatter.ISO_DATE), Priority.HIGH);
-		int taskId = taskList.createTask(newTask);
+		String taskId = taskList.createTask(newTask);
 		assertEquals(taskList.getNumberOfTasks(), 1);
 	}
 	
 	@Test
 	void testListTasksAfterCreateTask() {
 		Task newTask = new Task("Test Task List", "Implement tests for the task list manager", LocalDate.parse("2023-08-21", DateTimeFormatter.ISO_DATE), Priority.HIGH);
-		int taskId = taskList.createTask(newTask);
+		String taskId = taskList.createTask(newTask);
 		assertEquals(taskList.listTasks(), "Title: Test Task List\n"
 				+ "Description: Implement tests for the task list manager\n"
 				+ "Due date: 2023-08-21\n"
@@ -48,7 +48,7 @@ class TaksListTest {
 	@Test
 	void testTaskListSizeAfterDeleteTask() {
 		Task newTask = new Task("Test Task List", "Implement tests for the task list manager", LocalDate.parse("2023-08-21", DateTimeFormatter.ISO_DATE), Priority.HIGH);
-		int taskId = taskList.createTask(newTask);
+		String taskId = taskList.createTask(newTask);
 		assertEquals(taskList.getNumberOfTasks(), 1);
 		taskList.deleteTask(taskId);
 		assertEquals(taskList.getNumberOfTasks(), 0);
@@ -57,7 +57,7 @@ class TaksListTest {
 	@Test
 	void testListTasksAfterDeleteTask() {
 		Task newTask = new Task("Test Task List", "Implement tests for the task list manager", LocalDate.parse("2023-08-21", DateTimeFormatter.ISO_DATE), Priority.HIGH);
-		int taskId = taskList.createTask(newTask);
+		String taskId = taskList.createTask(newTask);
 		assertEquals(taskList.listTasks(), "Title: Test Task List\n"
 				+ "Description: Implement tests for the task list manager\n"
 				+ "Due date: 2023-08-21\n"
@@ -79,7 +79,7 @@ class TaksListTest {
 	@Test
 	void testUpdateCreatedTask() {
 		Task newTask = new Task("Test Task List", "Implement tests for the task list manager", LocalDate.parse("2023-08-21", DateTimeFormatter.ISO_DATE), Priority.HIGH);
-		int taskId = taskList.createTask(newTask);
+		String taskId = taskList.createTask(newTask);
 		assertEquals(taskList.listTasks(), "Title: Test Task List\n"
 				+ "Description: Implement tests for the task list manager\n"
 				+ "Due date: 2023-08-21\n"
@@ -95,9 +95,9 @@ class TaksListTest {
 	@Test
 	public void testListTasksByDateAndPriorityLevelEqual() {
 		Task task1 = new Task("Test Task List", "Implement tests for the task list manager", LocalDate.parse("2023-08-21", DateTimeFormatter.ISO_DATE), Priority.HIGH);
-		int taskId1 = taskList.createTask(task1);
+		String taskId1 = taskList.createTask(task1);
         Task task2 = new Task("Test Task", "Implement tests for the task entity", LocalDate.parse("2023-08-30", DateTimeFormatter.ISO_DATE), Priority.HIGH);
-        int taskId2 = taskList.createTask(task2);
+        String taskId2 = taskList.createTask(task2);
         assertEquals(taskList.listTasks(), "Title: Test Task List\n"
 				+ "Description: Implement tests for the task list manager\n"
 				+ "Due date: 2023-08-21\n"
@@ -111,9 +111,9 @@ class TaksListTest {
 	@Test
 	public void testListTasksByDateAndPriorityLevelNotEqual() {
 		Task task1 = new Task("Test Task List", "Implement tests for the task list manager", LocalDate.parse("2023-08-21", DateTimeFormatter.ISO_DATE), Priority.LOW);
-		int taskId1 = taskList.createTask(task1);
+		String taskId1 = taskList.createTask(task1);
         Task task2 = new Task("Test Task", "Implement tests for the task entity", LocalDate.parse("2023-08-30", DateTimeFormatter.ISO_DATE), Priority.HIGH);
-        int taskId2 = taskList.createTask(task2);
+        String taskId2 = taskList.createTask(task2);
         assertEquals(taskList.listTasks(), "Title: Test Task List\n"
 				+ "Description: Implement tests for the task list manager\n"
 				+ "Due date: 2023-08-21\n"
@@ -127,7 +127,7 @@ class TaksListTest {
 	@Test
 	public void testSetTaskPriority() {
 		Task task = new Task("Test Task List", "Implement tests for the task list manager", LocalDate.parse("2023-08-21", DateTimeFormatter.ISO_DATE), Priority.LOW);
-		int taskId = taskList.createTask(task);
+		String taskId = taskList.createTask(task);
 		assertEquals(taskList.listTasks(), "Title: Test Task List\n"
 				+ "Description: Implement tests for the task list manager\n"
 				+ "Due date: 2023-08-21\n"
