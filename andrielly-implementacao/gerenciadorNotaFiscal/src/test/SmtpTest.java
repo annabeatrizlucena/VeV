8e19519c1e09a51f94ae4736770a1baed55d7fd1
Andrielly Lucena <andrielly.lucena@ccc.ufcg.edu.br>
2023-08-19 15:06:00 -0300

    Adiciona classe de teste de Smtp

---
+++ andrielly-implementacao/gerenciadorNotaFiscal/src/test/SmtpTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import main.Fatura;
+import main.NotaFiscal;
+import util.Smtp;
+
+class SmtpTest {
+
+	@Test
+	void testSalvaNota() {
+		Fatura fatura = new Fatura("Jose Silva", "Rua Silva Barbosa, 975", "CONSULTORIA", 100.0);
+		
+		NotaFiscal notaFiscal = new NotaFiscal(fatura);
+		
+		assertEquals("salvando no banco", Smtp.envia(notaFiscal));
+	}
+
+}
