519b73f76d5e82c25214a6dd64ba2d0b4a17470b
Andrielly Lucena <andrielly.lucena@ccc.ufcg.edu.br>
2023-08-19 08:49:39 -0300

    Adiciona metodo de calculo de imposto

--- andrielly-implementacao/gerenciadorNotaFiscal/src/main/GeradorNotaFiscal.java
+++ andrielly-implementacao/gerenciadorNotaFiscal/src/main/GeradorNotaFiscal.java
@@ -2,9 +2,4 @@ package main;
 
 public class GeradorNotaFiscal {
 
-	public NotaFiscal geraNota(String nome, String endereco, String tipoServico, double valorFatura) {
-		NotaFiscal nota = new NotaFiscal(nome, endereco, tipoServico, valorFatura);
-		return nota;
-	}
-
 }
--- andrielly-implementacao/gerenciadorNotaFiscal/src/main/NotaFiscal.java
+++ andrielly-implementacao/gerenciadorNotaFiscal/src/main/NotaFiscal.java
@@ -3,9 +3,24 @@ package main;
 public class NotaFiscal {
 	
 	public boolean isValid;
+	private Fatura fatura;
 
 	public NotaFiscal(Fatura fatura) {
+		this.fatura = fatura;
 		isValid = true;
+	}
+
+	public double getImposto() {
+		double valorFatura = this.fatura.getValor();
+		String tipoServico = this.fatura.getTipoServico();
+		
+		if (tipoServico.equals("CONSULTORIA")) {
+			return valorFatura * 0.25;
+		} else if (tipoServico.equals("TREINAMENTO")) {
+			return valorFatura * 0.15;
+		} else {
+			return valorFatura * 0.06;
+		}
 	}
 
 }
