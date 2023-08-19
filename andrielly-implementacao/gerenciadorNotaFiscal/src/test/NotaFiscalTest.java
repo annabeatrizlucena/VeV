76c3cf44d733e41c124eaf0c0dfca48bcd9fe623
Andrielly Lucena <andrielly.lucena@ccc.ufcg.edu.br>
2023-08-19 08:29:15 -0300

    Adiciona teste de criacao de nota fiscal

---
+++ andrielly-implementacao/gerenciadorNotaFiscal/src/test/NotaFiscalTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import main.Fatura;
+import main.NotaFiscal;
+
+class NotaFiscalTest {
+	
+	@Test
+	void testNotaFiscalCreation() {
+		Fatura fatura = new Fatura("Jose Silva", "Rua Silva Barbosa, 975", "CONSULTORIA", 100.0);
+		
+		NotaFiscal notaFiscal = new NotaFiscal(fatura);
+		assertTrue(fatura.isValid);
+	}
+
+}
